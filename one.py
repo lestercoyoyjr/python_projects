f9b67d6cb7f965fc5940fa59a0ac51ed037be186
lestercch <lester_cch@hotmail.com>
2026-03-23 23:28:28 -0600

    Initial commit

---
+++ one.py
@@ -0,0 +1,33 @@
+print("Hello, World!\n");
+
+# This is a comment in Python. It is ignored by the interpreter.
+
+# VALUES
+
+'string'
+
+"string"
+
+"""
+This is a multi-line string."""
+
+'''
+This is also a multi-line string.'''
+
+
+40 # this is an integer
+
+40.2 # this is a float
+
+False # this is a boolean value
+
+True # this is also a boolean value
+
+# VARIABLES
+
+x = 40 # this is an integer variable
+
+a = 5
+b = 8
+c = a + b # this is an expression that adds a and b and assigns the result to c
+
