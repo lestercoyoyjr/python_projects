0f322570f00de6d683f142b02dc54416f1073297
Lester Confido <lester_cch@hotmail.com>
2026-04-05 23:04:13 -0600

    ADD operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,29 @@
+#AND
+resultado = True & True # Devolver True
+resultado2 = False & True # Devolver False
+resultado3 = True & False # Devolver False
+resultado4 = False & False # Devolver False
+
+#OR
+resultado5 = True | True # Devolver True
+resultado6 = False | True # Devolver True
+resultado7 = True | False # Devolver True
+resultado8 = False | False # Devolver False
+
+#NOT
+resultado9 = not True # Devolver False
+resultado10 = not False # Devolver True
+
+print("\nAND")
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+print("OR")
+print(resultado5)
+print(resultado6)
+print(resultado7)
+print(resultado8)
+print("NOT")
+print(resultado9)
+print(resultado10)
