efa122a5ffcb4d90ba12c5fd8817e57a559031e3
Lester Confido <lester_cch@hotmail.com>
2026-04-22 17:48:28 -0600

    ADD input by string and by numbers

---
+++ Inputs/input_nombres.py
@@ -0,0 +1,6 @@
+# pedirle un dato al usuario
+
+nombre = input("¿Cuál es tu nombre? ")
+print(f"Hola, {nombre}!")
+
+# siempre nos devuelve un string, aunque el usuario ingrese un número o cualquier otro tipo de dato, por lo que si queremos trabajar con números, debemos convertir el string a un tipo de dato numérico utilizando las funciones int() o float().
---
+++ Inputs/input_numeros.py
@@ -0,0 +1,8 @@
+# pedirle un numero al usuario
+
+numero = input("¿Cuál es tu número favorito? ")
+numero = int(numero)  # Convertir el string a un número entero
+print(f"Tu número favorito es: {numero}")
+numero = input("¿Cuál es tu número favorito como decimal? ")
+numero = float(numero)  # Convertir el número entero a un número decimal (float)
+print(f"Tu número favorito como decimal es: {numero}")
